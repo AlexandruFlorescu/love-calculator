e320bc964fa11111470ceb9e83bc7da17ae388ae
Alexandru Florescu <acdc152@gmail.com>
2018-07-21 10:43:59 +0300

    love-calculator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  firstName: string;
+  secondName: string;
+
+  get score(){
+    const letters = (this.firstName + this.secondName).toString().toLowerCase();
+    let sum = 0;
+    for (let i=0;i<letters.length; i++){
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
+  }
+
   constructor(public navCtrl: NavController) {
 
   }
